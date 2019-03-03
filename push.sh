7fd4d123405bf4f1e71e6ad8dff191fbca370117
Josh Cooke <josh.cooke@starlingbank.com>
2019-03-03 20:16:59 +0000

    release?

--- push.sh
+++ push.sh
@@ -7,4 +7,5 @@ git add .
 git commit -am "Release"
 echo $testparam
 git remote add azure https://$username:$password@joshcookedev.scm.azurewebsites.net/joshcookedev.git
+git pull azure master
 git push azure master
