a43240ac64566bdb6f1aa466cec4bb179d19b97c
Josh Cooke <josh.cooke@starlingbank.com>
2019-03-03 20:31:07 +0000

    tags

--- push.sh
+++ push.sh
@@ -4,6 +4,6 @@ set -e
 cd build
 git init
 git add .
-git commit -am "Release"
+git commit -am "$TRAVIS_BUILD_ID-$TRAVIS_BUILD_NUMBER"
 git remote add azure https://$username:$password@joshcookedev.scm.azurewebsites.net/joshcookedev.git
 git push azure master --force
